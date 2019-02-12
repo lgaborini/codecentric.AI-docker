3143d99de923ff04affcccaeace2dbbed8325534
Lorenzo Gaborini <lorenzo.gaborini@gmail.com>
2019-02-12 14:05:08 +0100

    Fix Docker Hub name

--- docker_push.sh
+++ docker_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push lgaborini/codecentric.AI-docker-avx
+docker push lgaborini/codecentric.ai-docker-avx
